4b3d6d615df895dbaf8eb56c3b0d29b65b284fd3
Bernie Dolan <bernarddolan@bernarddolan.dev>
2024-12-14 03:35:23 -0800

    Fix

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,16 @@ fn main() {
     let data = std::fs::read(&config.input_filename).unwrap();
     let line_len = config.columns * config.column_len;
     data.chunks(line_len).enumerate().for_each(|chunk| {
+        let printed_line = print_line(chunk.1, chunk.0, data.len(), &config);
+        println!("{printed_line}")
+    });
+}
+
+fn main_old() {
+    let config = args::Args::parse();
+    let data = std::fs::read(&config.input_filename).unwrap();
+    let line_len = config.columns * config.column_len;
+    data.chunks(line_len).enumerate().for_each(|chunk| {
         if !config.hide_ranges {
             let line_num = chunk.0;
             let line_num_len = (data.len().checked_ilog(16).unwrap_or(0) + 1) as usize;
@@ -35,6 +45,46 @@ fn main() {
         }
         println!();
     });
+}
+
+fn print_line(line: &[u8], line_num: usize, block_len: usize, config: &args::Args) -> String {
+    let mut printed_line = String::new();
+    if !config.hide_ranges {
+        let line_num_len = (block_len.checked_ilog(16).unwrap_or(0) + 1) as usize;
+        let line_start = line_num * line.len();
+        let line_end = std::cmp::min(line_start + line.len(), block_len);
+        printed_line.push_str(&format!("<{}-{}>   ", to_str_radix(line_start, 16, line_num_len), to_str_radix(line_end, 16, line_num_len)));
+    }
+    line.chunks(config.column_len).for_each(|column| {
+        column.iter().for_each(|b| {
+            printed_line.push_str(&to_str_radix(*b as usize, 16, 2));
+            printed_line.push(' ');
+        });
+        printed_line.push_str("  ");
+    });
+    if !config.no_decode {
+        let missing_byte_count = (config.columns * config.column_len) - line.len();
+        let missing_col_count = missing_byte_count / config.column_len;
+        if missing_byte_count != 0 {
+            let mut padding_string = String::new();
+            for _ in 0..missing_byte_count {
+                padding_string.push_str("   ");
+            }
+            for _ in 0..missing_col_count {
+                padding_string.push_str("  ");
+            }
+            printed_line.push_str(&padding_string);
+        }
+        line.iter().for_each(|b| {
+            let c = *b as char;
+            if c.is_alphanumeric() {
+                printed_line.push(c);
+            } else {
+                printed_line.push('.');
+            }
+        });
+    }
+    printed_line
 }
 
 fn to_str_radix(b: usize, radix: usize, padding: usize) -> String {
